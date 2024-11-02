f4e9c9b81893e5a474ed5620e436784120cd0bd9
cuthbert86 <c1027405@hallam.shu.ac.uk>
2024-11-02 06:38:10 +0000

    users app

--- users/models.py
+++ users/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
+    image = models.ImageField(default='media/profile_pics/default.jpeg',
+                              upload_to='profile_pics')
+   
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
