d9dd8fbdf2882b1f88122dea03f4610450bc41e7
cuthbert86 <c1027405@hallam.shu.ac.uk>
2024-11-02 06:40:27 +0000

    settings updated

--- itapps/settings.py
+++ itapps/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'itreporting',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'itreporting:home'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
