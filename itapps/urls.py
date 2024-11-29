704dd4dae76c5fa935b72bf13f6bd7a66e61de19
cuthbert86 <c1027405@hallam.shu.ac.uk>
2024-11-29 09:54:13 +0000

    updated to where my test one was working

--- itapps/urls.py
+++ itapps/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as views
-# from itreporting import views as itreporting_views 
+from users import views as user_views
+from itreporting import views 
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,16 +25,21 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('itreporting/', include('itreporting.urls')),
-    path('', include('itreporting.urls')),
-    path('register/', views.register, name='register'),
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('report',  views.report, name='report'),
+    path('/', include('itreporting.urls')),
+    path('contact', views.contact, name='contact'),
+#    path('', include('itreporting.urls')),
+    path('register/', user_views.register, name='register'),
     path('login/', 
          auth_views.LoginView.as_view(template_name='users/login.html'),
          name='login'),
-    path('logout/', 
+    path('logout/',
          auth_views.LogoutView.as_view(template_name='users/logout.html'),
          name='logout'),
-    path('profile', views.profile, name='profile'),
+    path('profile', user_views.profile, name='profile'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- itreporting/urls.py
+++ itreporting/urls.py
@@ -8,13 +8,13 @@ app_name = 'itreporting'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('itreporting/home/', views.home, name='home'),
-    path('itreporting/about/', views.about, name='about'),
-    path('ireporting/contact/', views.contact, name='contact'),
+    path('home', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('report/',  views.report, name='report'),
-    path('report/', PostListView.as_view(), name='report'),
+    path('reportlist/', PostListView.as_view(), name='reportlist'),
     path('issues/<int:pk>', PostDetailView.as_view(), name='issue-detail'),
     path('issue/new', PostCreateView.as_view(), name='issue-create'),
     path('issues/<int:pk>/update/', PostUpdateView.as_view(), name='issue-update'),
-    path('issue/<int:pk>/delete/', PostDeleteView.as_view(), name = 'issue-delete'),
+    path('issue/<int:pk>/delete/', PostDeleteView.as_view(), name='issue-delete'),
 ]
